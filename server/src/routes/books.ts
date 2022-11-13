f7d8b12cbe53c27ede651bd371658b8bcf695dd4
Zadkiel Romero <zadkielr@gmail.com>
2022-11-13 08:57:22 -0400

    test program

--- server/src/routes/books.ts
+++ server/src/routes/books.ts
@@ -1,32 +1,29 @@
 import express from "express";
-import * as booksServices from "../services/booksServices"
+import * as booksServices from "../services/booksServices";
 import toNewBookEntry from "../utils/utils";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
+  console.log("test");
   res.send(booksServices.getBooks());
 });
 
 router.get("/:year", (req, res) => {
-  const book =  booksServices.findByYear(Number(req.params.year))
-  
-  return (book != null) 
-    ? res.send(book)
-    : res.sendStatus(404);
+  const book = booksServices.findByYear(Number(req.params.year));
+
+  return book != null ? res.send(book) : res.sendStatus(404);
 });
 
 router.post("/", (req, res) => {
-
   try {
-    const newBook = toNewBookEntry(req.body)
-    const addedBookEntry = booksServices.addBook(newBook)
-  
-    res.json(addedBookEntry)
-  } catch(e) {
-    res.status(400).send(e)
-  }
+    const newBook = toNewBookEntry(req.body);
+    const addedBookEntry = booksServices.addBook(newBook);
 
+    res.json(addedBookEntry);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 export default router;
